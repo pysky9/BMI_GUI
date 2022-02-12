a76e10aea7723c70a6481d7a4d48639964426100
Derek Li <derek.j.h.li@gmail.com>
2022-02-12 16:39:30 +0800

    modifying the code

--- BMI.py
+++ BMI.py
@@ -1,7 +1,7 @@
-from tkinter import Tk
 from tkinter import Button
 from tkinter import Entry
 from tkinter import Label
+from tkinter import Tk
 
 root = Tk()
 root.title("BMI Calculator")
@@ -16,17 +16,22 @@ def BMI_calculate():
 
     try:
         global BMI_cal
+        global my_BMI
+        global my_health
         BMI_cal = weight_cal / ((height_cal / 100)) ** 2
-        BMI_result.insert(0, BMI_cal)
+
+        my_BMI = Label(root, text=f"{BMI_cal}")
+        my_BMI.grid(row=3, column=1)
 
         if BMI_cal >= 27:
-            health.insert(0, "肥胖")
+            my_health = Label(root, text="肥胖")
         elif BMI_cal >= 24 and BMI_cal < 27:
-            health.insert(0, "過重")
+            my_health = Label(root, text="過重")
         elif BMI_cal >= 18.5 and BMI_cal < 24:
-            health.insert(0, "正常")
+            my_health = Label(root, text="正常")
         else:
-            health.insert(0, "過輕")
+            my_health = Label(root, text="過輕")
+        my_health.grid(row=4, column=1)
 
     except:
         height_input.delete(0, "end")
@@ -35,16 +40,19 @@ def BMI_calculate():
 
 def clear():
     """clear all data that user key in when getting the information and user clicking the clear button"""
-    height_input.delete(0,"end")
+    global BMI_cal
+    global my_BMI
+    height_input.delete(0, "end")
     weight_input.delete(0, "end")
-    BMI_result.delete(0, "end")
-    health.delete(0, "end")
+    my_BMI.config(text="")
+    my_health.config(text="")
+
 
 # using Label to creating the name of column and pack it
-height = Label(root, text="請輸入身高(cm)",font=("Microsoft JhengHei",12,"bold"), justify="left")
-weight = Label(root, text="請輸入體重(kg)",font=("Microsoft JhengHei",12,"bold"), justify="left")
-BMI = Label(root, text="BMI result",font=("Microsoft JhengHei",12,"bold"), justify="left")
-health_comment = Label(root, text="健康資訊",font=("Microsoft JhengHei",12,"bold"), justify="left")
+height = Label(root, text="請輸入身高(cm)", font=("Microsoft JhengHei", 12, "bold"), justify="left")
+weight = Label(root, text="請輸入體重(kg)", font=("Microsoft JhengHei", 12, "bold"), justify="left")
+BMI = Label(root, text="BMI result", font=("Microsoft JhengHei", 12, "bold"), justify="left")
+health_comment = Label(root, text="健康資訊", font=("Microsoft JhengHei", 12, "bold"), justify="left")
 
 height.grid(row=0, column=0)
 weight.grid(row=1, column=0)
@@ -58,15 +66,9 @@ height_input.grid(row=0, column=1, padx=10, columnspan=2)
 weight_input = Entry(root)
 weight_input.grid(row=1, column=1, padx=10, columnspan=2)
 
-BMI_result = Entry(root)
-BMI_result.grid(row=3, column=1, padx=10, columnspan=2)
-
-health = Entry(root)
-health.grid(row=4, column=1, padx=10, columnspan=2)
-
 # Creating the button for calculating and clearing
-BMI_calculator = Button(root, text="BMI計算",font=("Microsoft JhengHei",12,"bold") ,command=BMI_calculate)
-Clear = Button(root, text="清除",font=("Microsoft JhengHei",12,"bold"), command=clear)
+BMI_calculator = Button(root, text="BMI計算", font=("Microsoft JhengHei", 12, "bold"), command=BMI_calculate)
+Clear = Button(root, text="清除", font=("Microsoft JhengHei", 12, "bold"), command=clear)
 
 BMI_calculator.grid(row=2, column=0)
 Clear.grid(row=2, column=1)
